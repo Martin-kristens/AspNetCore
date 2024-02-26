817551407528f3f83a61a832f298e987d77a7226
Martin <martinkristensen12@gmail.com>
2024-02-26 11:46:34 +0100

    created authController

---
+++ AspNetCore.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            ViewData["Title"] = "Sign Ip";
+            return View();
+        }
+
+        public IActionResult SignUn()
+        {
+            ViewData["Title"] = "Sign Up";
+            return View();
+        }
+
+        public new IActionResult SignOut()
+        {
+            //när man sign out kommer man automatiskt till HomeControllern 
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
