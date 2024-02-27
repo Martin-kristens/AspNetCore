bab9ea0e7b0064de45b8b99095505f8ef68139f3
Martin <martinkristensen12@gmail.com>
2024-02-27 15:02:40 +0100

    changed styling and using parts

--- AspNetCore.Mvc/ViewModels/Authentication/SignUpViewModel.cs
+++ AspNetCore.Mvc/ViewModels/Authentication/SignUpViewModel.cs
@@ -4,7 +4,7 @@ namespace AspNetCore.Mvc.ViewModels.Authentication;
 
 public class SignUpViewModel
 {
-    public string Title { get; set; } = "hello";
+    public string Title { get; set; } = "Sign Up";
     public SignUpModel Form { get; set; } = new SignUpModel();
 
     public bool TermAndConditions { get; set; } = true;
