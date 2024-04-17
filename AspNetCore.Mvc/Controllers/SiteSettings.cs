9353ce7805650a8abf27a51fe3b41695a170c406
Martin <martinkristensen12@gmail.com>
2024-04-17 18:56:36 +0200

    cookieConsent done

---
+++ AspNetCore.Mvc/Controllers/SiteSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Mvc.Controllers;
+
+public class SiteSettings : Controller
+{
+    //public IActionResult Index()
+    //{
+    //    return View();
+    //}
+
+    [HttpPost]
+    public IActionResult CookieConsent()
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddYears(1),
+            HttpOnly = true,
+            Secure = true
+        };
+        Response.Cookies.Append("CookieConsent", "true", option);
+        return Ok();
+    }
+}
